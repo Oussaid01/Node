b0bef104f6eff4277294941c920322813a0b6f63
Oussaid <oussaid.dridi.0@gmail.com>
2025-12-11 23:09:04 +0100

    done

---
+++ src/controllers/recipeController.js
@@ -0,0 +1,69 @@
+import Recipe from "../models/Recipe.js"
+
+
+export const getAllRecipies = async (_,res)=>{
+  try{
+      const recipies = await Recipe.find().sort({createdAt:-1})
+      res.status(200).json(recipes)
+  } catch(error){
+      console.error("Error in getAllRecipies controller: ",error)
+      res.status(500).json({message : "Internal server error "})
+  }
+}
+
+
+export const getRecipeById = async (req,res)=>{
+  try{
+      const recipe = await Recipe.findById(req.params.id)
+      if (!recipe) {return res.status(404).json({message : 'Recipe not found'})}
+      res.status(200).json(recipe)
+  } catch(error){
+      console.error("Error in getRecipeById controller: ",error)
+      res.status(500).json({message : "Internal server error "})
+  }
+}
+
+
+
+
+
+export async function createRecipe (req,res) {
+  try{
+      const {title, content} = req.body
+      const newRecipe = new Recipe({title,content})
+      const savedRecipe = await newRecipe.save()
+      res.status(201).json(savedRecipe)
+  }catch(error){
+      console.error("Error in createRecipe controller: ",error)
+      res.status(500).json({message : "Internal server error "})
+  }
+}
+
+export async function updateRecipe (req,res) {
+  try{
+      const {title , content} = req.body
+      const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id,{title, content},{new:true,})
+
+      if (!updatedRecipe){
+          return res.status(404).json({message : "Recipe not found"})
+      }
+
+      res.status(200).json({message : "Recipe updated successfully",updatedRecipe})
+  }catch(error){
+      console.error("Error in updateRecipe controller: ",error)
+      res.status(500).json({message : "Internal server error "})
+  }
+}
+
+
+
+export async function deleteRecipe (req,res) {
+  try{  
+      const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id)
+      if (!deletedRecipe){return res.status(404).json({message : 'Recipe not found'})}
+     res.status(200).json({message : "u just deleted the recipe"})
+  }catch(error){
+      console.error("Error in deleteRecipe controller: ",error)
+      res.status(500).json({message : "Internal server error "})
+  }
+}
---
+++ src/routes/recipeRoutes.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { createRecipe, deleteRecipe, getAllRecipies, getRecipeById } from "../controllers/recipeController.js"
+
+const router = express.Router()
+
+
+
+router.get("/", getAllRecipies)
+router.get("/:id", getRecipeById)
+router.post("/", createRecipe)
+router.put("/:id", updateRecipe)
+router.delete("/:id", deleteRecipe)
+
+
+
+export default router;
